3029b1362d545d432c42c189edaf622bbac194d8
tj-bu-708-PC15 <tj-bu-708-PC15@DESKTOP-OLEBFQ2>
2025-01-17 18:11:26 +0900

    2025-01-17

--- java_example/src/day16/DBConn.java
+++ java_example/src/day16/DBConn.java
@@ -1,11 +1,24 @@
-package day15;
+package day16;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBConn {
+	public static void printData(ResultSet rs) throws SQLException {
+
+		while(rs.next()) {
+			String name = rs.getString("STU_NAME");
+			int height = rs.getInt("STU_HEIGHT");
+
+			System.out.print(name+"\t");
+			System.out.println(height);
+					
+		}
+
+	}
 	public static void main(String[] args) {
 		Connection conn;
 		Statement stmt = null;
@@ -16,8 +29,11 @@ public class DBConn {
 			String db_pw = "test1234";
 			conn = DriverManager.getConnection(db_url, db_id, db_pw);
 			System.out.println("DB 연결 완료");
-			
-			
+			stmt = conn.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT * FROM STUDENT");
+			printData(rs);
+				
 		} catch (ClassNotFoundException e) {
 			System.out.println("JDBC 드라이버 로드 오류");
 		} catch (SQLException e) {
